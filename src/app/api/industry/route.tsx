a84858c7476dcd0b16726be88941e647dbed78c0
nuzulasrar <80189595+nuzulasrar@users.noreply.github.com>
2023-09-26 16:32:31 +0800

    fix any

--- src/app/api/industry/route.tsx
+++ src/app/api/industry/route.tsx
@@ -1,6 +1,9 @@
 
 import { NextResponse } from "next/server"
 
-export async function GET(request: any, { params }) {
+
+export async function GET(request: any, { params }: any) {
+    console.log(params);
+
     return new NextResponse(JSON.stringify("hahaha"))
 }
