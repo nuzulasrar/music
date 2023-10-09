dd20cd6a0377f94fce8271d41f2392edfc95d9f8
nuzulasrar <80189595+nuzulasrar@users.noreply.github.com>
2023-10-09 21:48:13 +0800

    latest

--- src/app/api/industry/route.tsx
+++ src/app/api/industry/route.tsx
@@ -1,22 +1,9 @@
 
 import { NextResponse } from "next/server"
 
-import prisma from "../../lib/prisma"
-import { PrismaClient } from "@prisma/client"
-
 
 export async function GET(request: any, { params }: any) {
-    // console.log(params);
+    console.log(params);
 
-    // return new NextResponse(JSON.stringify("hahaha"))
-    try {
-        const prisma2 = new PrismaClient()
-    } catch (error: any) {
-        alert(error.message)
-    }
-
-
-
-
-
+    return new NextResponse(JSON.stringify("hahaha"))
 }
