80d874cd2284d88d299150c381839184ebd95546
nuzulasrar <80189595+nuzulasrar@users.noreply.github.com>
2023-11-01 14:46:28 +0800

    completed damage type declarations

--- src/constants/components.js
+++ src/constants/components.js
@@ -286,3 +286,547 @@ export const damage13 = [
     details: "If detected, rating shall be 4",
   },
 ];
+
+export const damage14 = [
+  {
+    level: "LIGHT",
+    details: "If detected area measure < 2%, rating shall be 1",
+    details_2: "- *Area is measured along the abutment/pier",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected, rating shall be 4",
+    details_2:
+      "If detected area measure up to 25%, rating shall be 2 face and 1 meter below the bearing shelf",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected, rating shall be 4",
+    details_2: "If detected area measure 25% - 50%, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected, rating shall be 4",
+    details_2: "If detected area > 50%, rating shall be 4",
+  },
+];
+
+export const damage15 = [
+  {
+    level: "LIGHT",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected, rating shall be 4",
+  },
+];
+export const damage16 = [
+  {
+    level: "LIGHT",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected, rating shall be 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected, rating shall be 4",
+  },
+];
+export const damage17 = [
+  {
+    level: "LIGHT",
+    details:
+      "If undermining and loss of support up to 5% of foundation, rating shall be 1",
+    details_2: "-",
+    details_3: "Minor scour at inlet/outlet of culvert, scour depth < 200mm",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "If undermining and loss of support between 5 to 15% of foundation, rating shall be 2",
+    details_2:
+      "Scour area measuring 10% - 20% in any direction, rating shall be 2",
+    details_3:
+      "Significant scour at inlet/outlet of culvert, scour depth between 200mm - 300mm",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "If undermining and loss of support between 15% to 20% of foundation, rating shall be 3",
+    details_2:
+      "Scour area measuring 20% - 30% in any direction, rating shall be 3",
+    details_3:
+      "Severe scour at inlet/outlet of culvert, scour depth between 300mm - 500mm",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "If undermining and loss of support between of > 20% of foundation, rating shall be 4",
+    details_2: "Scour area measuring > 30% in any direction, rating shall be 4",
+    details_3:
+      "Extensive washout around inlet/outlet with loss of embankment fill, scour depth > 500mm",
+  },
+];
+export const damage18 = [
+  {
+    level: "LIGHT",
+    details: "If detected, rating shall be 3",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected, rating shall be 3",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected, rating shall be 3",
+  },
+];
+export const damage19 = [
+  {
+    level: "LIGHT",
+    details: "-",
+  },
+  {
+    level: "MEDIUM",
+    details: "If only debris detected, rating shall be 3",
+  },
+  {
+    level: "SEVERE",
+    details: "If debris with vegetation detected, rating shall be 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "-",
+  },
+];
+export const damage20 = [
+  {
+    level: "LIGHT",
+    details: "Blockage of pipe up to 25%",
+  },
+  {
+    level: "MEDIUM",
+    details: "Blockage of pipe 25% - 50%",
+  },
+  {
+    level: "SEVERE",
+    details: "Blockage of pipe 50% - 75%",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Blockage of pipe > 75%",
+  },
+];
+export const damage21 = [
+  {
+    level: "LIGHT",
+    details: "No water mark observed",
+  },
+  {
+    level: "MEDIUM",
+    details: "Slight water mark observed",
+  },
+  {
+    level: "SEVERE",
+    details: "Water spilled directly on facade",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Very wet surface",
+  },
+];
+export const damage22 = [
+  {
+    level: "LIGHT",
+    details: "Parapet/Body intact",
+  },
+  {
+    level: "MEDIUM",
+    details: "Minor indentation to parapet/body",
+  },
+  {
+    level: "SEVERE",
+    details: "Severely damaged parapet/body",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Parapet totally shifted out of position",
+  },
+];
+export const damage23 = [
+  {
+    level: "LIGHT",
+    details: "Holes measuring < 150mm in any direction or 25mm in depth",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "Holes measuring 150mm - 300mm in any direction or between 25mm - 50mm in depth",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "Holes measuring 300mm - 600mm in any direction or between 50mm - 100mm in depth",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Holes measuring > 600mm in any direction or > 100mm deep",
+  },
+];
+export const damage24 = [
+  {
+    level: "LIGHT",
+    details: "Less than 10mm deep",
+  },
+  {
+    level: "MEDIUM",
+    details: "From 10mm - 20mm deep",
+  },
+  {
+    level: "SEVERE",
+    details: "From 20mm - 40mm deep",
+  },
+  {
+    level: "V.SEVERE",
+    details: "> 40mm deep",
+  },
+];
+export const damage25 = [
+  {
+    level: "LIGHT",
+    details: "Delaminated area measuring < 150mm in any direction",
+  },
+  {
+    level: "MEDIUM",
+    details: "Delaminated area measuring 150mm - 300mm in any direction",
+  },
+  {
+    level: "SEVERE",
+    details: "Delaminated area measuring 300mm - 600mm in any direction",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Delaminated area measuring > 600mm in any direction",
+  },
+];
+export const damage26 = [
+  {
+    level: "LIGHT",
+    details: "Few noticeable bumps",
+  },
+  {
+    level: "MEDIUM",
+    details: "Several bumps producing a rough ride",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "Numerous bumps producing a very rough ride with possible loss of vehicle control",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "Numerous bumps producing a very rough ride with imminent danger of loss of vehicle control",
+  },
+];
+export const damage27 = [
+  {
+    level: "LIGHT",
+    details:
+      "If alligator cracks cover an area of less than 10% within the same span rating shall be 1",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "If alligator cracks cover an area of 10% to 15% within the same span rating shall be 2",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "If alligator cracks cover an area of 15% to 20% within the same span rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "If alligator cracks cover an area of more than 20% within the same span rating shall be 4",
+  },
+];
+export const damage28 = [
+  {
+    level: "LIGHT",
+    details: "less than 5mm wide single or multiple cracks",
+  },
+  {
+    level: "MEDIUM",
+    details: "5mm-10mm wide single or multiple cracks",
+  },
+  {
+    level: "SEVERE",
+    details: "10mm-15mm wide single or multiple cracks",
+  },
+  {
+    level: "V.SEVERE",
+    details: "> 15mm wide single or multiple cracks",
+  },
+];
+export const damage29 = [
+  {
+    level: "LIGHT",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected rating shall be 4",
+  },
+];
+export const damage30 = [
+  {
+    level: "LIGHT",
+    details: "Difference in level < 10mm at expansion gap/bridge approach",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "Difference in level between 10mm - 20mm at expansion gap/bridge approach",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "Difference in level between 20mm - 30mm at expansion gap/bridge approach",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Difference in level > 30mm at expansion gap/bridge approach",
+  },
+];
+
+export const damage31 = [
+  {
+    level: "LIGHT",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "MEDIUM",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "SEVERE",
+    details: "If detected rating shall be 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If detected rating shall be 4",
+  },
+];
+export const damage32 = [
+  {
+    level: "LIGHT",
+    details: "Fine or hair line crack detected on rubber seal",
+  },
+  {
+    level: "MEDIUM",
+    details: "Medium crack detected on rubber seal",
+  },
+  {
+    level: "SEVERE",
+    details: "Wide or large crack detected on rubber seal",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Rubber seal dislodge or peel off from location",
+  },
+];
+export const damage33 = [
+  {
+    level: "LIGHT",
+    details: "Severe crack, abnormal buldge, worn out and aged, rating = 4",
+  },
+  {
+    level: "MEDIUM",
+    details: "Severe crack, abnormal buldge, worn out and aged, rating = 4",
+  },
+  {
+    level: "SEVERE",
+    details: "Severe crack, abnormal buldge, worn out and aged, rating = 4",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Severe crack, abnormal buldge, worn out and aged, rating = 4",
+  },
+];
+export const damage34 = [
+  {
+    level: "LIGHT",
+    details: "-",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "If bearing has been displaced by less than 10%, rating shall be 2",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "If bearing has been displaced by between 10% - 20%, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "If bearing has been displaced by more than 20%, rating shall be 4",
+  },
+];
+export const damage35 = [
+  {
+    level: "LIGHT",
+    details:
+      "Less than 5% loss of materials at embankments directly supporting foundation, rating shall be 1",
+    details_2:
+      "Less than 10% loss of materials at embankments, rating shall be 1",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "5% to 15% loss of materials at embankments directly supporting foundation, rating shall be 2",
+    details_2: "10% to 30% loss of materials at embankments, rating shall be 2",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "15% to 20% loss of materials at embankments directly supporting foundation, rating shall be 3",
+    details_2: "30% to 40% loss of materials at embankments, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "More than 20% loss of materials at embankments directly supporting foundation, rating shall be 4",
+    details_2:
+      "More than 40% loss of materials at embankments, rating shall be 1",
+  },
+];
+export const damage36 = [
+  {
+    level: "LIGHT",
+    details: "Hairline cracks and minor loss of stones to a depth of 50mm deep",
+  },
+  {
+    level: "MEDIUM",
+    details: "Narrow cracks and loss of stones between 50mm and 100mm deep",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "Spalling and disintegration of stones with section loss between 100mm to 150mm",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "Extensive loss of stones and disintegration endangering the stability of the structure",
+  },
+];
+export const damage37 = [
+  {
+    level: "LIGHT",
+    details: "Silting covered < 20% of culvert height",
+  },
+  {
+    level: "MEDIUM",
+    details: "Silting covered from 20% - 30% of culvert height",
+  },
+  {
+    level: "SEVERE",
+    details: "Silting covered from 30% - 50% of culvert height",
+  },
+  {
+    level: "V.SEVERE",
+    details: "Silting covered from > 50% of culvert height",
+  },
+];
+export const damage38 = [
+  {
+    level: "LIGHT",
+    details:
+      "1:1 Ratio of abutment spacing/culvert size to the size of rivers/streams, rating shall be 1",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "1:1.5 Ratio of abutment spacing/culvert size to the size of rivers/streams, rating shall be 2",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "1:2 Ratio of abutment spacing/culvert size to the size of rivers/streams, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "> 1:2 Ratio of abutment spacing/culvert size to the size of rivers/streams, rating shall be 4",
+  },
+];
+export const damage39 = [
+  {
+    level: "LIGHT",
+    details:
+      "Damage area or missing panel number is less than 5% of ceiling or roof area",
+  },
+  {
+    level: "MEDIUM",
+    details:
+      "Damage area or missing panel number is 5% to 10% of ceiling or roof area",
+  },
+  {
+    level: "SEVERE",
+    details:
+      "Damage area or missing panel number is 10% to 15% of ceiling or roof area",
+  },
+  {
+    level: "V.SEVERE",
+    details:
+      "Damage area or missing panel number more than 15% of ceiling or roof area",
+  },
+];
+export const damage40 = [
+  {
+    level: "LIGHT",
+    details: "-",
+  },
+  {
+    level: "MEDIUM",
+    details: "-",
+  },
+  {
+    level: "SEVERE",
+    details: "If only loose detected, rating shall be 3",
+  },
+  {
+    level: "V.SEVERE",
+    details: "If missing material detected, rating shall be 4",
+  },
+];
