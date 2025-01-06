12fafb8ed2c73d618e10b01cbc40b5ce82dd5d89
nuzulasrar <80189595+nuzulasrar@users.noreply.github.com>
2025-01-06 12:48:19 +0800

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
             <Image src="/logo.png" alt="" width={75} height={75} />
           </div>
           <h1 className="text-white font-semibold text-[25px]">
-            47- Prestasi Perintis Inspection System
+            48- Prestasi Perintis Inspection System
           </h1>
           <div className="">
             <Image
