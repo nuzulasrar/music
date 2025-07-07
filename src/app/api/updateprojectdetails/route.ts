7e1cc0332634cc2eda89a97638e40db915bdfa55
nuzulasrar <nuzulasrar@gmail.com>
2025-07-08 02:11:34 +0800

    feat: Added update project detail API

--- src/app/api/updateprojectdetails/route.ts
+++ src/app/api/updateprojectdetails/route.ts
@@ -1,26 +1,38 @@
 import { NextResponse } from "next/server";
 import prisma from "../../lib/prisma";
+import { log } from "console";
 
 export const revalidate = 0;
 
 export async function PUT(request: any) {
   try {
-    const findProjectDetails = await prisma.projectBridge.findFirst({
-      where: { id: 1 },
+    const { projectID, formdataa } = await request.json();
+
+    const READ_ProjectBridgeDetail = await prisma.projectBridge.findFirst({
+      where: { id: projectID },
     });
 
-    if (findProjectDetails) {
-      return NextResponse.json({
-        success: true,
-        msg: "Successfully updated project details.",
+    if (READ_ProjectBridgeDetail) {
+      const UPDATE_ProjectBridge = await prisma.projectBridge.update({
+        where: { id: projectID },
+        data: formdataa,
       });
+      if (UPDATE_ProjectBridge) {
+        return NextResponse.json({
+          success: true,
+          msg: formdataa,
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          error: "Fail to update project detail to database.",
+        });
+      }
     } else {
-      return NextResponse.json({
-        success: false,
-        error: "Fail to update project detail to database.",
-      });
     }
   } catch (error: any) {
+    console.log(error);
+
     return NextResponse.json({
       error: error.message,
     });
